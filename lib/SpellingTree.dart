87ee59909a599347479b23701de60ffb94dca281
Evan Loo <evanhsie@usc.edu>
2024-11-09 00:32:56 -0800

    Final commit? Answers popup, scores & levels, backspace button. Fixed validWords and initializeGame

--- lib/SpellingTree.dart
+++ lib/SpellingTree.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hexagon/hexagon.dart';
-import 'dart:math';
 import 'SpellingTree_cubit.dart';
-import 'SpellingTree_state.dart';
+import 'dart:math';
 
 void main() {
   runApp(SpellingTreeApp());
@@ -28,89 +27,190 @@ class SpellingTreeScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Spelling Tree'),
-        actions: [
-          BlocBuilder<SpellingCubit, SpellingState>(
-            builder: (context, state) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Center(child: Text('Score: ${state.score}')),
-              );
-            },
-          ),
-        ],
       ),
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          final centerX = constraints.maxWidth / 2;
-          final centerY = constraints.maxHeight / 2 - 50; // Adjusted for buttons
+      body: Row(
+        children: [
+          Expanded(
+            child: LayoutBuilder(
+              builder: (context, constraints) {
+                final maxWidth = constraints.maxWidth;
+                final maxHeight = constraints.maxHeight;
 
-          return Column(
-            children: [
-              BlocBuilder<SpellingCubit, SpellingState>(
-                builder: (context, state) {
-                  return Center(
-                    child: Container(
-                      width: 200, // Set a fixed width for the TextField
-                      child: TextField(
-                        readOnly: true,
-                        maxLength: 10, // Limit the length of the TextField
-                        decoration: InputDecoration(
-                          hintText: state.currentWord,
-                          counterText: '', // Hide the counter text
+                return Center(
+                  child: Container(
+                    height: 500, // Set a fixed height for the row
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center, // Center children vertically
+                      children: [
+                        BlocBuilder<SpellingCubit, SpellingState>(
+                          builder: (context, state) {
+                            // Update the thresholds list
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 30.0), // Add horizontal padding
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text('Score: ${state.score}', style: TextStyle(fontSize: 18)),
+                                  Column(
+                                    children: [
+                                      Row(
+                                        children: List.generate(5, (index) {
+                                          return Icon(
+                                            index < state.level ? Icons.star : Icons.star_border,
+                                            color: Colors.yellow, size: 20,
+                                          );
+                                        }),
+                                      ),
+                                      Text(
+                                        state.level < 5
+                                            ? 'Level ${state.level+1} at ${state.thresholds[state.level+1]} points'
+                                            : 'Max level reached',
+                                        style: TextStyle(fontSize: 18),
+                                      ),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
                         ),
-                      ),
+                        SizedBox(height: 20), // Add a SizedBox for spacing
+                        BlocBuilder<SpellingCubit, SpellingState>(
+                          builder: (context, state) {
+                            return Center(
+                              child: Container(
+                                width: 250, // Set a fixed width for the TextField and button
+                                child: Row(
+                                  children: [
+                                    Expanded(
+                                      child: TextField(
+                                        readOnly: true,
+                                        maxLength: 10, // Limit the length of the TextField
+                                        decoration: InputDecoration(
+                                          hintText: state.currentWord.toUpperCase(),
+                                          counterText: '', // Hide the counter text
+                                        ),
+                                      ),
+                                    ),
+                                    IconButton(
+                                      icon: Icon(Icons.backspace, size: 15,),
+                                      onPressed: () => context.read<SpellingCubit>().backspaceWord(),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                        ),
+                        BlocBuilder<SpellingCubit, SpellingState>(
+                          builder: (context, state) {
+                            return Container(
+                              height: 20, // Fixed height for the message area
+                              child: state.message.isNotEmpty
+                                  ? Text(
+                                state.message,
+                                style: TextStyle(color: Colors.red),
+                              )
+                                  : Container(),
+                            );
+                          },
+                        ),
+                        Expanded(
+                          child: Center(
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                for (int i = 0; i < 6; i++)
+                                  Positioned(
+                                    left: maxWidth / 2 + 70 * cos((i * pi / 3) + (pi / 6)) - 40, // Adjusted for centering
+                                    top: maxHeight / 2 + 70 * sin((i * pi / 3) + (pi / 6)) - 220, // Adjusted for centering
+                                    child: HexagonButton(index: i, isCenter: false),
+                                  ),
+                                Positioned(
+                                  left: maxWidth / 2 - 40, // Adjusted for centering
+                                  top: maxHeight / 2 - 220, // Adjusted for centering
+                                  child: HexagonButton(index: 6, isCenter: true),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () => context.read<SpellingCubit>().checkWord(),
+                              child: Text('Check'),
+                            ),
+                            ElevatedButton(
+                              onPressed: () => context.read<SpellingCubit>().shuffleLetters(),
+                              child: Text('Shuffle'),
+                            ),
+                            ElevatedButton(
+                              onPressed: () => context.read<SpellingCubit>().clearWord(),
+                              child: Text('Clear'),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                  );
-                },
-              ),
-              Expanded(
-                child: Center(
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      for (int i = 0; i < 6; i++)
-                        Positioned(
-                          left: centerX + 90 * cos((i * pi / 3) + (pi / 6)) - 60, // Adjusted for centering
-                          top: centerY + 90 * sin((i * pi / 3) + (pi / 6)) -100, // Adjusted for centering
-                          child: HexagonButton(index: i, isCenter: false),
-                        ),
-                      Positioned(
-                        left: centerX - 60, // Adjusted for centering
-                        top: centerY - 100, // Adjusted for centering
-                        child: HexagonButton(index: 6, isCenter: true),
-                      ),
-                    ],
                   ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 100.0),
-                child: Row(
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    ElevatedButton(
-                      onPressed: () => context.read<SpellingCubit>().checkWord(),
-                      child: Text('Check'),
+                    Text(
+                      'Found Words',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
                     ElevatedButton(
-                      onPressed: () => context.read<SpellingCubit>().shuffleLetters(),
-                      child: Text('Shuffle'),
-                    ),
-                    ElevatedButton(
-                      onPressed: () => context.read<SpellingCubit>().clearWord(),
-                      child: Text('Clear'),
+                      onPressed: () => context.read<SpellingCubit>().showAnswers(context),
+                      child: Text('Show answers'),
                     ),
                   ],
                 ),
-              ),
-            ],
-          );
-        },
+                SizedBox(height: 30), // Add a SizedBox for spacing
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
+                      ),
+                      child: BlocBuilder<SpellingCubit, SpellingState>(
+                        builder: (context, state) {
+                          return GridView.builder(
+                            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 3, // Number of columns
+                              crossAxisSpacing: 10, // Spacing between columns
+                              mainAxisSpacing: 10, // Spacing between rows
+                            ),
+                            itemCount: state.foundWords.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(state.foundWords[index]),
+                              );
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
 }
-
 class HexagonButton extends StatelessWidget {
   final int index;
   final bool isCenter;
@@ -122,14 +222,14 @@ class HexagonButton extends StatelessWidget {
     return BlocBuilder<SpellingCubit, SpellingState>(
       builder: (context, state) {
         return GestureDetector(
-          onTap: () => context.read<SpellingCubit>().selectLetter(state.letters[index]),
+          onTap: () => context.read<SpellingCubit>().selectLetter(state.letters[index].toUpperCase()),
           child: HexagonWidget.flat(
-            width: 70,
+            width: 80,
             color: isCenter ? Colors.grey : Colors.blue,
             cornerRadius: 8.0,
             child: Center(
               child: Text(
-                state.letters[index],
+                state.letters[index].toUpperCase(),
                 style: TextStyle(color: Colors.white, fontSize: 20),
               ),
             ),
--- lib/SpellingTree_cubit.dart
+++ lib/SpellingTree_cubit.dart
@@ -1,29 +1,239 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'dart:math';
-
-import  'SpellingTree_state.dart';
+import 'package:all_english_words/all_english_words.dart';
+import 'package:flutter/material.dart';
 
 class SpellingCubit extends Cubit<SpellingState> {
-  SpellingCubit() : super(SpellingState.initial());
+  final AllEnglishWords _englishWords = AllEnglishWords();
+
+  SpellingCubit() : super(SpellingState.initial()) {
+    _initializeGame();
+  }
+
+
+  Future<void> _initializeGame() async {
+    final word = await _findRandomWordWith7UniqueAlphabets();
+    List<String> letters = word.split('').toSet().toList();
+
+    letters.shuffle(Random());
+    final validWords = await getValidWords(letters);
+    int maxScore = validWords.fold(0, (sum, word) => sum + calculateWordScore(word));
+    print("Max score: $maxScore");
+    // Set thresholds
+    final thresholds = [
+      0,
+      (maxScore * 0.02).round(),
+      (maxScore * 0.05).round(),
+      (maxScore * 0.08).round(),
+      (maxScore * 0.11).round(),
+      (maxScore * 0.15).round(),
+    ];
+
+    print("Letters: $letters");
+    print("Valid words: $validWords");
+    emit(state.copyWith(letters: letters, validWords: validWords, thresholds: thresholds));
+  }
+
+  Future<String> _findRandomWordWith7UniqueAlphabets() async {
+    final random = Random();
+    String word;
+    final allWords = await _englishWords.allWords;
+    do {
+      word = allWords[random.nextInt(allWords.length)].toLowerCase();
+    } while (word.contains('-') || word.length < 7 || word.split('').toSet().length != 7);
+    return word;
+  }
 
   void selectLetter(String letter) {
     emit(state.copyWith(currentWord: state.currentWord + letter));
   }
 
   void clearWord() {
-    emit(state.copyWith(currentWord: ''));
+
+    emit(state.copyWith(message: '', currentWord: ''));
   }
 
+
+  void backspaceWord() {
+    if (state.currentWord.isNotEmpty) {
+      final updatedWord = state.currentWord.substring(0, state.currentWord.length - 1);
+      emit(state.copyWith(currentWord: updatedWord));
+    }
+  }
   void shuffleLetters() {
-    List<String> shuffledLetters = List.from(state.letters);
-    shuffledLetters.shuffle(Random());
+    List<String> firstSixLetters = state.letters.sublist(0, 6);
+    firstSixLetters.shuffle(Random());
+    List<String> shuffledLetters = [...firstSixLetters, state.letters.last];
     emit(state.copyWith(letters: shuffledLetters));
   }
 
   void checkWord() {
-    // Implement dictionary check and scoring logic here
-    // For now, just clear the word
-    emit(state.copyWith(currentWord: ''));
+
+    final centerLetter = state.letters.last; // Assuming the center letter is at index 6
+    final isValidWord = state.validWords.contains(state.currentWord.toLowerCase());
+    final usesCenterLetter = state.currentWord.toLowerCase().contains(centerLetter);
+
+    if (state.currentWord.isEmpty) {
+      emit(state.copyWith(message: 'Enter a word'));
+    } else if (state.currentWord.length < 4) {
+      emit(state.copyWith(message: 'Word must be at least 4 letters'));
+    } else if (!usesCenterLetter) {
+      emit(state.copyWith(message: 'Word must use the center letter $centerLetter'));
+    } else if (!isValidWord) {
+      emit(state.copyWith(message: 'Invalid word'));
+    } else if (state.foundWords.contains(state.currentWord)) {
+      emit(state.copyWith(message: 'Word already entered'));
+    } else {
+      final newScore = state.score + calculateWordScore(state.currentWord);
+      final newLevel = calculateLevel(newScore);
+      emit(state.copyWith(
+        foundWords: List.from(state.foundWords)..add(state.currentWord),
+        currentWord: '',
+        score: newScore,
+        level: newLevel,
+        nextLevelThreshold: newLevel < 6 ? state.thresholds[newLevel] : 0,
+        message: '',
+      ));
+    }
   }
+
+  int calculateWordScore(String word) {
+    int score = word.length == 4 ? 1 : word.length;
+    if (word.split('').toSet().length == 7) { // Check if the word is a pangram
+      score += 7;
+    }
+    return score;
+  }
+
+  int calculateLevel(int score) {
+
+    print("Score: $score");
+    if(score >= state.thresholds.last){
+      int level = state.thresholds.length -1 ;
+      print("Level: $level");
+
+      return level;
+    }
+    int level = state.thresholds.indexWhere((threshold) => score < threshold) - 1;
+    print("Level: $level");
+    return level;
+  }
+
+  Future<List<String>> getValidWords(List<String> letters) async {
+    final centerLetter = letters[6];
+    final allWords = await _englishWords.allWords;
+    final List<String> validWords = allWords.where((word) {
+      return word.length >= 4 && word.contains(centerLetter) && word.split('').toSet().every((char) => letters.contains(char));
+    }).toList();
+    return validWords;
+  }
+
+  void showAnswers(BuildContext context) {
+    final remainingWords = state.validWords.where((word) => !state.foundWords.contains(word)).toList();
+    // emit(state.copyWith(message: 'Words you missed:'));
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Words Missed'),
+          content: SingleChildScrollView(
+            child: Column(
+              children: [
+                for (int i = 0; i < (remainingWords.length / 5).ceil(); i++)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      for (int j = 0; j < 5; j++)
+                        if (i * 5 + j < remainingWords.length) ...[
+                          Expanded(
+                            child: Text(remainingWords[i * 5 + j]),
+                          ),
+                          if (j < 4) SizedBox(width: 10), // Add spacing between columns
+                        ],
+                    ],
+                  ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+// In SpellingState class
+
+class SpellingState extends Equatable {
+  final List<String> letters;
+  final String currentWord;
+  final int score;
+  final List<String> validWords;
+  final List<String> foundWords;
+  final String message;
+  final int level;
+  final int nextLevelThreshold;
+  final List<int> thresholds;
+
+  SpellingState({
+    required this.letters,
+    required this.currentWord,
+    required this.score,
+    required this.validWords,
+    required this.foundWords,
+    required this.message,
+    required this.level,
+    required this.nextLevelThreshold,
+    required this.thresholds,
+  });
+
+  factory SpellingState.initial() {
+    return SpellingState(
+      letters: [],
+      currentWord: '',
+      score: 0,
+      validWords: [],
+      foundWords: [],
+      message: '',
+      level: 0,
+      nextLevelThreshold: 10,
+      thresholds: [0, 10, 15, 20, 25, 30],
+    );
+  }
+
+  SpellingState copyWith({
+    List<String>? letters,
+    String? currentWord,
+    int? score,
+    List<String>? validWords,
+    List<String>? foundWords,
+    String? message,
+    int? level,
+    int? nextLevelThreshold,
+    List<int>? thresholds,
+  }) {
+    return SpellingState(
+      letters: letters ?? this.letters,
+      currentWord: currentWord ?? this.currentWord,
+      score: score ?? this.score,
+      validWords: validWords ?? this.validWords,
+      foundWords: foundWords ?? this.foundWords,
+      message: message ?? this.message,
+      level: level ?? this.level,
+      nextLevelThreshold: nextLevelThreshold ?? this.nextLevelThreshold,
+      thresholds: thresholds ?? this.thresholds,
+    );
+  }
+
+  @override
+  List<Object> get props => [letters, currentWord, score, validWords, foundWords, message, level, nextLevelThreshold, thresholds];
 }
